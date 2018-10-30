903aaac1df768f06990185365b6c6f56cdd17942
Asib <32617031+ALasib@users.noreply.github.com>
2018-10-29 23:36:25 -0700

    Create left alignment.cpp

---
+++ left alignment.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+string str;
+vector<int>v;
+
+int main()
+{
+    getline(cin,str);
+    int len=str.size();
+    int fs;
+    cin>>fs;
+    string s="";
+    int now=fs-1;
+    while(now<len)
+    {
+        while(isalnum(str[now]))
+            now--;
+        v.push_back(now);
+        now+=fs;
+    }
+
+    int j=0;
+    for(int i=0;i<len;i++)
+    {
+        if(i==v[j])
+        {
+            s+='\n';
+            j++;
+        }
+        else
+            s+=str[i];
+    }
+
+    cout<<s;
+    return 0;
+}
